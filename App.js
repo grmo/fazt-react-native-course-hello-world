bd98dc9517d0c57b941b34ade7931783df2a6bbc
grmo <43529985+grmo@users.noreply.github.com>
2020-12-20 16:14:04 -0500

    Add project files

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  Text,
+  View,
+  StyleSheet
+} from "react-native";
+
+const App = () => {
+  return ( <
+    View style = {
+      styles.container
+    } >
+    <
+    Text style = {
+      styles.title
+    } > Hello World!! < /Text>  < /
+    View >
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#292929"
+  },
+  title: {
+    fontSize: 30,
+    color: "#fff"
+  }
+})
+
+export default App;
